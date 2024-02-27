ed89b4f37343612b5dae89981c1a56efc76f8730
mksmvnv <mksmvnv@icloud.com>
2024-02-28 01:20:20 +0300

    Created check file with all tests

---
+++ src/tests/tests.h
@@ -0,0 +1,13 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include "../fake_string.h"
+
+#define SIZE 15
+
+void test_memchr();
+void test_memcmp();
+void test_memcpy();
+void test_strlen();
+
+#endif  // TESTS_H
