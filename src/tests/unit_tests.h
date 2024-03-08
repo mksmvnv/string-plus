f63fd2e163c90e83b09e2c2658ea6eca373b34d0
mksmvnv <mksmvnv@icloud.com>
2024-03-08 15:18:25 +0300

    Created unit tests for fake string.h funcs

---
+++ src/tests/unit_tests.h
@@ -0,0 +1,12 @@
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+
+#include <check.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../fake_string.h"
+
+Suite *test_fake_str();
+
+#endif  // UNIT_TESTS_H
